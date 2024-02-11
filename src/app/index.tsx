7ea9161c32a8640dfb76cf08ce2bdebeb940c051
Sávio <saviopf22@gmail.com>
2024-02-11 20:58:34 -0300

    criação header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from "@/components/header";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 py-8">
+    <View className="flex-1">
+      <Header title="Faça seu pedido" />
       <Text className="font-heading text-2xl text-white">iae</Text>
     </View>
   );
