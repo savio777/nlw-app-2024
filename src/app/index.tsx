9e0acbb03ea20500c247cadf0aeb40797ff785ec
Sávio <saviopf22@gmail.com>
2024-02-11 21:26:04 -0300

    criação dos primeiros componentes da home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,42 @@
+import { FlatList, View, ListRenderItem } from "react-native";
+
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { Text, View } from "react-native";
+
+import { CATEGORIES } from "@/utils/data/products";
+import { useCallback, useState } from "react";
 
 export default function Home() {
+  const [categorySelected, setCategorySelected] = useState(CATEGORIES[0]);
+
+  const handleChangeCategory = useCallback((item: string) => {
+    setCategorySelected(item);
+  }, []);
+
+  const renderItem: ListRenderItem<string> = useCallback(
+    ({ item }) => (
+      <CategoryButton
+        title={item}
+        isSelected={categorySelected === item}
+        onPress={() => handleChangeCategory(item)}
+      />
+    ),
+    [categorySelected]
+  );
+
   return (
     <View className="flex-1">
       <Header title="Faça seu pedido" />
-      <Text className="font-heading text-2xl text-white">iae</Text>
+
+      <FlatList
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 4, paddingHorizontal: 20 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item}
+        data={CATEGORIES}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
